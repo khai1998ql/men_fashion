20ea206ede6605f81d588c6c69b9664cc17cb0e9
Khai <khainguyensi.1998@gmail.com>
2021-06-05 17:38:42 +0700

    kk

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -12,20 +12,21 @@ class FrontendController extends Controller
 {
     public function index(){
         $productNew = DB::table('products')->where('hot_new', 1)
-                    ->join('categories', 'products.category_id', 'categories.id')
-                    ->join('subcategories', 'products.subcategory_id', 'subcategories.id')
+                    ->join('categories', 'products.category_id', '=' ,'categories.id')
+                    ->join('subcategories', 'products.subcategory_id', '=' , 'subcategories.id')
                     ->where('products.product_status', 1)
                     ->select('products.*', 'categories.category_name', 'subcategories.subcategory_name')
                     ->orderBy('products.id')
                     ->limit(12)
                     ->get();
         $productHot = DB::table('products')
-                    ->join('categories', 'products.category_id', 'categories.id')
-                    ->join('subcategories', 'products.subcategory_id', 'subcategories.id')
+                    ->join('categories', 'products.category_id', '=' , 'categories.id')
+                    ->join('subcategories', 'products.subcategory_id', '=' , 'subcategories.id')
                     ->where('products.product_status', 1)
                     ->select('products.*', 'categories.category_name', 'subcategories.subcategory_name')
                     ->orderBy('products.product_sold')
-                    ->limit(12)->get();
+                    ->limit(12)
+                    ->get();
         return view('frontend.pages.index', compact('productNew', 'productHot'));
     }
     public function error(){
